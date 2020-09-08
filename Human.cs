3805289ffbc1d8d83e145a8a26f9d7bedaeb2556
Alexandr Sorokin <alex18andr@mail.ru>
2020-09-08 22:39:37 +0300

    first commit

---
+++ Human.cs
@@ -0,0 +1,35 @@
+using System;
+namespace intro_to_vscode
+{
+    public class Human
+    {
+        public int Age {get; set;}
+        public string Name {get; set;}
+        public string Sex {get; set;}
+        public string Main{get; set;}
+
+        public void HumanSay()
+        {
+            Console.WriteLine("Здарова, бандиты!");
+        }
+
+        public Human()
+        {
+
+        }
+        public Human(string name)
+        {
+            Name = name;
+        }
+
+        public Human(string name, int age) : this(name)
+        {
+            Age = age;
+        }
+
+        public Human(string name, int age, string sex) : this (name, age)
+        {
+            Sex = sex;
+        }
+    }
+}
